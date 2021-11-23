38d93f19ac2adf3564db8e228f065295f3b44db8
camfairchild <cameron.fairchild@mail.utoronto.ca>
2021-11-23 10:25:27 -0500

    fixed naming

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const port = 3000;
 app.use(express.static('pub'));
 
 // redirect to example.html
-app.get('/', (req, res) => res.redirect('/example.html'));
+app.get('*', (req, res) => res.redirect('/examples.html'));
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}!`)
